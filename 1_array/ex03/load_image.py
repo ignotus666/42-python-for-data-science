f05c322f42545903df77dc71367d3da5f6bda9a8
ignotus666 <darylo1@hotmail.com>
2026-05-21 08:09:18 +0200

    Add

---
+++ 1_array/ex03/load_image.py
@@ -0,0 +1,29 @@
+import numpy as np
+from PIL import Image
+
+
+def ft_load(path: str) -> np.ndarray:
+    """
+    Loads an image, prints its format/shape,
+    and returns its RGB pixel content as a numpy array.
+    """
+    try:
+        assert isinstance(path, str) and len(path) > 0, \
+            "Path must be a valid string."
+        assert path.lower().endswith(('.jpg', '.jpeg', '.png')), \
+            "Unsupported format."
+        
+        img = Image.open(path)
+        
+        img_rgb = img.convert("RGB")
+        res_array = np.array(img_rgb)
+
+        print(f"The shape of image is: {res_array.shape}")
+        print(res_array)
+
+        return res_array
+
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+    except Exception as e:
+        print(f"Exception: {e}")
---
+++ 1_array/ex03/zoom.py
@@ -0,0 +1,17 @@
+from load_image import ft_load
+from load_image import ft_load
+
+
+def main():
+    try:
+        
+
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+    except Exception as e:
+        print(f"Exception: {e}")
+
+
+
+if __name__ == "__main__":
+    main()
