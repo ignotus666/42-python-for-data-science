4eda65313e1dbf0b8e9977cc3dadb6642a95feba
Daryl Hanlon <dhanlon@c3r5s6.42urduliz.com>
2026-05-21 12:03:40 +0200

    Add ex03

--- 1_array/ex03/zoom.py
+++ 1_array/ex03/zoom.py
@@ -1,16 +1,21 @@
 from load_image import ft_load
-from load_image import ft_load
+import matplotlib.pyplot as plt
 
 
 def main():
     try:
-        
+        starting_img = ft_load("animal.jpeg")
+        print(starting_img)
+
+        zoomed_img = starting_img[40:490, 440:940, 0]
+        print(f"New shape after slicing: {zoomed_img.shape} or {zoomed_img.shape[:2]}")
+        plt.imshow(zoomed_img, "gray")
+        plt.show()
 
     except AssertionError as e:
         print(f"AssertionError: {e}")
     except Exception as e:
         print(f"Exception: {e}")
-
 
 
 if __name__ == "__main__":
