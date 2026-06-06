869d7946d1c2830df51d0d01e2caf8715c1d0788
ignotus666 <darylo1@hotmail.com>
2026-06-06 21:12:25 +0200

    upload

--- 2_data_table/ex02/aff_pop.py
+++ 2_data_table/ex02/aff_pop.py
@@ -5,9 +5,14 @@ from load_csv import load
 
 
 def convert_vals(val: str):
-    val_int = val.replace({"K":"*1e3", "M":"*1e6", "B":"*1e9"}, regex=True)\
-        .map(pd.eval).astype(int)
-    return(val_int)
+    if val.endswith("k"):
+        return float(val[:-1]) * 1e3
+    elif val.endswith("M"):
+        return float(val[:-1]) * 1e6
+    elif val.endswith("B"):
+        return float(val[:-1]) * 1e9
+    else:
+        return float(val)
 
 def main():
     try:
@@ -16,8 +21,11 @@ def main():
             raise AssertionError("Could not load data from file")
         spain_data = data.loc["Spain"]
         france_data = data.loc["France"]
-        spain_data.index = convert_vals(spain_data.index)
-        france_data.index = convert_vals(france_data.index)
+        spain_data.index = spain_data.index.astype(int)
+        france_data.index = france_data.index.astype(int)
+        spain_data = spain_data.loc[:2050].map(convert_vals)
+        france_data = france_data.loc[:2050].map(convert_vals)
+        max_pop = max(max(spain_data), max(france_data))
     except AssertionError as e:
         print("AssertionError:", e)
     except KeyError as e:
@@ -28,23 +36,19 @@ def main():
         print("ValueError:", e)
 
     else:
-        # 3. Plotting setup
-        spain_data.plot()
-        france_data.plot()
-        # 4. Set exact X-axis range and steps (1800 to 2080 in steps of 40)
-        # Note: stop=2081 ensures 2080 is included in the labels
-        plt.xlim(1780, 2060)
-        plt.xticks(np.arange(1800, 2041, 40))
+        spain_data.plot(label="Spain")
+        france_data.plot(label="France")
+        
+        plt.xlim(1790, 2060)
+        plt.xticks(range(1800, 2051, 40))
+        
+        plt.ylim(0, max_pop + 10000000)
+        plt.yticks(range(1, 81000000, 20000000))
 
-        # 5. Set exact Y-axis range and steps (30 to 90 in steps of 10)
-        # Note: stop=91 ensures 90 is included in the labels
-        plt.ylim(0, 80000000)
-        plt.yticks(np.arange(0, 81000000, 20000000))
-
-        # 6. Labels and display
         plt.title("Population Projections")
         plt.xlabel("Year")
         plt.ylabel("Population")
+        plt.legend(loc="lower right")
         plt.show()
 
 
